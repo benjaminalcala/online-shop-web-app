8cffb104f15a5b23411487032004de4b02a174a8
Benjamin Alcala <benalcalajr@gmail.com>
2020-01-07 13:24:38 -0800

    fixing stripe url

--- controllers/shop.js
+++ controllers/shop.js
@@ -242,8 +242,8 @@ exports.getCheckout = (req, res, next) => {
       }),
       // success_url: 'http://localhost:3000/checkout/success',
       // cancel_url: 'http://localhost:3000/checkout/cancel'
-      success_url: req.protocol + '://' + req.get('host') + 'checkout/success',
-      cancel_url: req.protocol + '://' + req.get('host') + 'checkout/cancel'
+      success_url: req.protocol + '://' + req.get('host') + '/checkout/success',
+      cancel_url: req.protocol + '://' + req.get('host') + '/checkout/cancel'
     })
   })
     .then(session => {
